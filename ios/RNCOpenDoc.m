4adb330648d260db573c38f61e990e97efefa1a4
Guy Blank <guy.blank@capriza.com>
2018-09-03 14:03:39 +0300

    ios add support for pick - opens UIDocumentPickerViewController

--- ios/RNCOpenDoc.m
+++ ios/RNCOpenDoc.m
@@ -3,6 +3,12 @@
 #if __has_include("NSTiffSplitter.h")
 #import "NSTiffSplitter.h"
 #endif
+#if __has_include("RCTConvert.h")
+#import "RCTConvert.h"
+#else
+#import <React/RCTConvert.h>
+#endif
+#import <MobileCoreServices/UTCoreTypes.h>
 
 @interface RNCOpenDoc ()
 
@@ -10,7 +16,10 @@
 
 @end
 
-@implementation RNCOpenDoc
+@implementation RNCOpenDoc {
+    RCTResponseSenderBlock pickCallback;
+    NSMutableArray *pickResponse;
+}
 
 RCT_EXPORT_MODULE();
 
@@ -43,7 +52,7 @@ RCT_EXPORT_METHOD(open: (NSURL *)path)
 {
     self.documentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:path];
     self.documentInteractionController.delegate = self;
-    
+
     UIViewController *root = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
     CGRect screenRect = [[root view] bounds];
     CGFloat screenWidth = screenRect.size.width;
@@ -51,7 +60,7 @@ RCT_EXPORT_METHOD(open: (NSURL *)path)
     BOOL fileOpenSuccess = [self.documentInteractionController
                             presentOptionsMenuFromRect:CGRectMake((screenWidth / 2), screenHeight, 1, 1)
                             inView:[root view] animated:YES];
-    
+
     if (!fileOpenSuccess) {
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"No suitable Apps installed"
                                                         message:@"You don't seem to have any other Apps installed that can open this document."
@@ -62,12 +71,86 @@ RCT_EXPORT_METHOD(open: (NSURL *)path)
     }
 }
 
+RCT_EXPORT_METHOD(pick:(NSDictionary *)options callback:(RCTResponseSenderBlock)callback)
+{
+    NSArray *allowedUTIs = @[(NSString*)kUTTypeContent];
+    if (options) {
+        allowedUTIs = [RCTConvert NSArray:options[@"fileTypes"]];
+    }
+    UIDocumentPickerViewController *docPicker = [[UIDocumentPickerViewController alloc] initWithDocumentTypes:allowedUTIs inMode:UIDocumentPickerModeImport];
+
+    //Set the delegate
+    docPicker.delegate = self;
+    //present the document picker
+    UIViewController *rootViewController = [[[[UIApplication sharedApplication]delegate] window] rootViewController];
+
+    pickCallback = callback;
+
+    [rootViewController presentViewController:docPicker animated:YES completion:nil];
+}
+
+- (void)getResultFromUrl:(NSURL *)url error:(NSError **)outError {
+    [url startAccessingSecurityScopedResource];
+
+    NSFileCoordinator *coordinator = [[NSFileCoordinator alloc] init];
+
+    [coordinator coordinateReadingItemAtURL:url options:NSFileCoordinatorReadingResolvesSymbolicLink error:outError byAccessor:^(NSURL *newURL) {
+        NSMutableDictionary* result = [NSMutableDictionary dictionary];
+        [result setValue:newURL.absoluteString forKey:@"uri"];
+        [result setValue:[newURL lastPathComponent] forKey:@"fileName"];
+        [pickResponse addObject:result];
+    }];
+
+    [url stopAccessingSecurityScopedResource];
+}
+
+
 #pragma mark -
 #pragma mark Document Interaction Controller Delegate Methods
 
 - (UIViewController *) documentInteractionControllerViewControllerForPreview: (UIDocumentInteractionController *) controller
 {
     return [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+}
+
+#pragma mark -
+#pragma mark UIDocumentPickerDelegate
+- (void)documentPicker:(UIDocumentPickerViewController *)controller didPickDocumentAtURL:(NSURL *)url {
+    if (controller.documentPickerMode == UIDocumentPickerModeImport) {
+        pickResponse = [NSMutableArray arrayWithCapacity:1];
+         __block NSError *error;
+        [self getResultFromUrl:url error:&error];
+        if (error) {
+            pickCallback(@[[error localizedDescription], pickResponse]);
+        }
+        else {
+            pickCallback(@[[NSNull null], pickResponse]);
+        }
+    }
+}
+
+- (void)documentPicker:(UIDocumentPickerViewController *)controller didPickDocumentsAtURLs:(NSArray <NSURL *>*)urls {
+    if (controller.documentPickerMode == UIDocumentPickerModeImport) {
+        pickResponse = [NSMutableArray arrayWithCapacity:urls.count];
+        __block NSError *error;
+        for (NSURL *url in urls) {
+            [self getResultFromUrl:url error:&error];
+            if (error) {
+                break;
+            }
+        }
+
+        if (error) {
+            pickCallback(@[[error localizedDescription], pickResponse]);
+        }
+        else {
+            pickCallback(@[[NSNull null], pickResponse]);
+        }
+    }
+}
+
+- (void)documentPickerWasCancelled:(UIDocumentPickerViewController *)controller {
+    pickCallback(@[[NSNull null], [NSNull null]]);
 }
 
 
